de6f2b69734d32f581dbedb32813cb0d2845ca5f
Derian Rodriguez <dmrodriguez2000@gmail.com>
2022-08-02 14:19:27 -0600

    Fixed button

--- src/components/AdviceButton.jsx
+++ src/components/AdviceButton.jsx
@@ -3,7 +3,7 @@ import iconDice from '../assets/images/icon-dice.svg';
 export const AdviceButton = ({ onChangeAdvice, isLoading }) => {
   return (
     <>
-      <button type="submit" className='change-advice' onClick={ () => onChangeAdvice() } disabled={ isLoading }>
+      <button className='change-advice' onClick={ () => onChangeAdvice() } disabled={ isLoading }>
         <img src={ iconDice } alt="Change Advice" />
       </button>
     </>
