14325db192c16c63b31519681b282594b0a9a77f
Derian Rodriguez <dmrodriguez2000@gmail.com>
2022-08-02 16:02:54 -0600

    Fixed accessibility issue

--- src/AdviceGeneradorApp.jsx
+++ src/AdviceGeneradorApp.jsx
@@ -1,15 +1,9 @@
-import { AdviceButton } from './components/AdviceButton';
 import { AdviceCard } from './components/AdviceCard';
-import { useFetchAdvice } from './hooks/useFetchAdvice';
 
 export const AdviceGeneradorApp = () => {
-
-  const { advice, onChangeAdvice, isLoading } = useFetchAdvice();
-
   return (
     <>
-      <AdviceCard { ...advice } />
-      <AdviceButton onChangeAdvice={ onChangeAdvice } isLoading={ isLoading }/>
+      <AdviceCard />
       <footer className="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
         Coded by <a href="#">Derian Martín Rodríguez Durán</a>.
--- src/components/AdviceCard.jsx
+++ src/components/AdviceCard.jsx
@@ -1,16 +1,27 @@
+import { useFetchAdvice } from '../hooks/useFetchAdvice';
+import { AdviceButton } from './AdviceButton';
 import patternDividerD from '../assets/images/pattern-divider-desktop.svg';
 import patternDividerM from '../assets/images/pattern-divider-mobile.svg';
 
-export const AdviceCard = ({ id, advice }) => {
+export const AdviceCard = () => {
+
+  const { advice:info, onChangeAdvice, isLoading } = useFetchAdvice();
+  const { id, advice } = info;
+
   return (
     <>
       <main className='advice-card'>
-        <h1 className="advice-title"><span>Advice</span> #{ id }</h1>
-        <p className='advice-body'>"{ advice }"</p>
-        <picture>
-          <source media="(min-width: 450px)" srcSet={ patternDividerD } />
-          <img className='advice-divider' src={ patternDividerM } alt="" />
-        </picture>
+        <div className="advice-container">
+          <h1 className="advice-title"><span>Advice</span> #{ id }</h1>
+          <p className='advice-body'>"{ advice }"</p>
+          <picture>
+            <source media="(min-width: 450px)" srcSet={ patternDividerD } />
+            <img className='advice-divider' src={ patternDividerM } alt="" />
+          </picture>
+        </div>
+        <div className='button-container'>
+          <AdviceButton onChangeAdvice={ onChangeAdvice } isLoading={ isLoading }/>
+        </div>
       </main>
     </>
   )
